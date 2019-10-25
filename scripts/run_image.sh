1358d5facebfec3f479cb950000e527b5ba7eee2
SoroushVT <svafaiet>
2019-10-25 19:16:23 +0330

    Changed run and stoping in docker into docker api, refactoring

--- scripts/run_image.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# run a docker with $1 name made from image $2
-docker run -name $1 -p 8000:8000 --rm -it $2
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import subprocess
 
-def run_cmd(cmd: str, directory: str):
+def run_cmd(cmd: str, directory: str="."):
     pr = subprocess.Popen(cmd , cwd=directory, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, error = pr.communicate()
     return out, error
