09a300133d32351d170a62bedc852f8594a6f842
SoroushVT <svafaiet>
2019-11-04 17:34:45 +0330

    Add configuration.py again

---
+++ configuration.py
@@ -0,0 +1,20 @@
+HOST = '0.0.0.0'
+PORT = 9098
+TEST_TIMEOUT_S = 60
+RUN_TIMEOUT_S = 60
+ACCESS_TIMEOUT_S = 5
+
+REPORT_SERVER_HOST = '127.0.0.1'
+REPORT_SERVER_PORT = 8000
+REPORT_SERVER_PATH = 'contest/team/judge-result'
+
+TEST_FILES_PATH = '/home/soroushvt/code/webelopers/w_test'
+TEST_MODULE = "main_func.py"
+
+MAX_MEMORY_CONTESTANT = 50
+
+BASE_DIR = "/home/soroushvt/code/webelopers/judge/"
+
+PORT_COUNTER_START = 9000
+
+REPO_PATH = "/home/soroushvt/code/webelopers/repositories"
