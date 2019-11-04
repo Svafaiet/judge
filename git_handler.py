4e735c9c747569c021a28d9921f0516795c5e479
SoroushVT <svafaiet>
2019-11-04 22:04:32 +0330

    Bug fix in git handler exception handlers and logs

--- git_handler.py
+++ git_handler.py
@@ -3,13 +3,16 @@ import re
 from utils import run_cmd
 import logger
 
+GIT_CLONE_ERROR_REGEX = "Please make sure you have the correct access rights"
+GIT_CHECKOUT_ERROR_REGEX = "Aborting"
+
 
 def _run_git(directory: str, git_cmd: str):
     return run_cmd(cmd="/usr/bin/" + git_cmd, directory=directory)
 
 
-def update_git(root_dir: str, group_id: int, git_url: str, branch_name="master"):
-    folder_name = "g" + str(group_id)
+def update_git(root_dir: str, group_id: str, git_url: str, branch_name="master"):
+    folder_name = "g_" + group_id
     logger.log_info("updating git for group {} with git url {} initiated".format(group_id, git_url))
     group_dir = os.path.join(root_dir, folder_name)
     if not os.path.exists(group_dir):
@@ -24,12 +27,12 @@ def update_git(root_dir: str, group_id: int, git_url: str, branch_name="master")
         logger.log_error("Could not find project name in {}".format(git_url))
         raise Exception("Unable to find git url")
     if not os.path.exists(os.path.join(group_dir, project_name)):
-        logger.log_info("cloning into repository {}".format(git_url))
+        logger.log_info("Cloning into repository {}".format(git_url))
         clone_cmd = "git clone " + git_url
         out, error = _run_git(group_dir, clone_cmd)
         if len(out) != 0:
             logger.log_info("In cloning process for group {}: {}".format(group_id, out))
-        if len(error) != 0:
+        if len(error) != 0 and re.match(GIT_CLONE_ERROR_REGEX, str(error)):
             logger.log_log("Cloning into repository {} failed with error: {}".format(git_url, str(error)))
             raise Exception("Failed while Cloning the Project")
     else:
@@ -37,9 +40,8 @@ def update_git(root_dir: str, group_id: int, git_url: str, branch_name="master")
     project_dir = os.path.join(group_dir, project_name)
     checkout_cmd = "git checkout " + branch_name
     out, error = _run_git(project_dir, checkout_cmd)
-    if len(error) != 0:
+    if len(error) != 0 and re.match(GIT_CHECKOUT_ERROR_REGEX, str(error)):
         logger.log_info("Failed to checkout {} in project {}".format(branch_name, project_name))
-        raise Exception("Failed while checking out")
     pull_cmd = "git pull origin"
     out, error = _run_git(project_dir, pull_cmd)
     if len(error) != 0:
