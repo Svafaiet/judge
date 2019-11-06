87ed4f2b6fc4ab2cb73536985d2192b223d46fe0
SoroushVT <svafaiet>
2019-11-06 13:46:16 +0330

    Remove migrations from user projects, Add makemigrations

--- project_handler.py
+++ project_handler.py
@@ -32,6 +32,7 @@ class ContestantProjectHandler:
         except Exception:
             logger.log_error("Could not copy django settings for group {}".format(group_id))
             raise Exception("Could not copy django settings")
+        out, error = run_cmd(cmd="./scripts/remove_migrations.sh " + project_dir, directory=".")  # TODO handle logs
         out, error = run_cmd(cmd="./scripts/build_image.sh " + project_dir, directory=".")
         build_msg = out.decode("utf-8")
         logger.log_info("Project for group {} build successfully with message: {}".format(group_id, build_msg))
---
+++ scripts/remove_migrations.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+find $1 -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find $1 -path "*/migrations/*.pyc"  -delete
