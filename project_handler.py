af960476075bb1769fa08c9e12cc4fb2a2435aeb
SoroushVT <svafaiet>
2019-11-06 12:39:48 +0330

    Add logs and exception handling in project handler

--- project_handler.py
+++ project_handler.py
@@ -7,6 +7,13 @@ import os
 from shutil import copyfile
 from configuration import MAX_MEMORY_CONTESTANT, BASE_DIR
 
+import logger
+
+SUCCESSFUL_STEP_REGEX = r"(\s*--->[^-]+)+"
+COPY_REQUIREMENTS_REGEX = r"COPY requirements\.txt $DIR/"
+INSTALL_REQUIREMENTS_REGEX = r"RUN pip install -r requirements\.txt"
+ERROR_REGEX = r"ERROR:"
+
 
 class ContestantProjectHandler:
     CONTESTANT_SETTINGS_NAME = "server_settings.py"
@@ -15,31 +22,44 @@ class ContestantProjectHandler:
     def __init__(self):
         self.client = docker.from_env()
 
-    def setup(self, repo_dir: str, group_id: int, git_url: str):
+    def setup(self, repo_dir: str, group_id: str, git_url: str):
         project_dir = update_git(root_dir=repo_dir, group_id=group_id, git_url=git_url)
-
-        copyfile(
-            ContestantProjectHandler.CONTESTANT_SETTINGS_PATH,
-            os.path.join(project_dir, ContestantProjectHandler.CONTESTANT_SETTINGS_NAME)
-        )
-
+        try:
+            copyfile(
+                ContestantProjectHandler.CONTESTANT_SETTINGS_PATH,
+                os.path.join(project_dir, ContestantProjectHandler.CONTESTANT_SETTINGS_NAME)
+            )
+        except Exception:
+            logger.log_error("Could not copy django settings for group {}".format(group_id))
+            raise Exception("Could not copy django settings")
         out, error = run_cmd(cmd="./scripts/build_image.sh " + project_dir, directory=".")
         build_msg = out.decode("utf-8")
-#        TODO: check different error types: requirements.txt, ...
+        logger.log_info("Project for group {} build successfully with message: {}".format(group_id, build_msg))
         try:
             image_id = re.search(r"Successfully built ((\w|\d)+)\n", build_msg).group(1)
-        except:
-            logger.log_error("failed to build docker image. exiting.")
-            exit(1)
+        except Exception:
+            if re.findall(COPY_REQUIREMENTS_REGEX, build_msg) is not None and re.findall(
+                    INSTALL_REQUIREMENTS_REGEX, build_msg) is None:
+                logger.log_error("Could not find requirements.txt for group {}".format(group_id))
+                raise Exception("Could not find requirements.txt file")
 
-        os.remove(os.path.join(project_dir, ContestantProjectHandler.CONTESTANT_SETTINGS_NAME))
+            if re.findall(INSTALL_REQUIREMENTS_REGEX + SUCCESSFUL_STEP_REGEX + ERROR_REGEX, build_msg) is not None:
+                logger.log_error("Could not install requirements for group {}".format(group_id))
+                raise Exception("Could not install requirements")
+            logger.log_error("Failed to build docker image for group {}.".format(group_id))
+            raise Exception("Failed to build docker image")
 
-        print(image_id)
-        # handle error requirements.txt: no such file or directory
+        try:
+            os.remove(os.path.join(project_dir, ContestantProjectHandler.CONTESTANT_SETTINGS_NAME))
+        except Exception:
+            logger.log_error("Could not remove django settings for group {}".format(group_id))
+            raise Exception("Could not remove django settings")
+
+        logger.log_success("Image built for team {} successfully".format(group_id))
         return image_id
 
     def run(self, image_id: str, port: int):
-        container_name = "webelopers" + str(uuid.uuid1())
+        container_name = "webelopers_" + str(port)
         result = self.client.containers.run(
             image=image_id,
             detach=True,
@@ -51,21 +71,13 @@ class ContestantProjectHandler:
             ports={"8000": port},
 
         )
-        # run_cmd(cmd="./scripts/run_container.sh " + container_name + " " + image_id, directory=".")
-        print("server_is_up")
-        print(result)
+        logger.log_success(
+            "Project is running on container {} with id {} for image {}".format(container_name, result, image_id))
         return container_name
 
     def kill(self, container_name: str):
         self.client.containers.get(container_name).stop()
-        # return run_cmd(cmd="./scripts/stop_container.sh " + container_name, directory=".")
+        logger.log_info("Killing container {}...".format(container_name))
 
 
 DEFAULT_PROJECT_HANDLER = ContestantProjectHandler()
-
-# import os
-#
-# proj = ContestantProjectHandler()
-# img_hash = proj.setup(os.environ['HOME'] + "/temp", 10, "git@git.edu.sharif.edu:webelopers2019/terminator.git")
-# container_name = proj.run(img_hash, 8000)
-# proj.kill(img_hash)
